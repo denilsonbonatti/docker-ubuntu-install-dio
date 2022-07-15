1415210d024ba0cf834c9f7733ce3227be33c12a
Denilson Bonatti <denilsonbonatti@gmail.com>
2022-07-15 10:48:19 -0300

    Create install-docker-ubuntu.sh

---
+++ install-docker-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Instalando o Docker......."
+
+apt-get update -y
+apt-get install ca-certificates curl gnupg lsb-release -y
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
